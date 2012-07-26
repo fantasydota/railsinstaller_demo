75afe34fc6b91533e40b81cda2e648aeb88d5b84
Han Chen <fantasy.dota2@gmail.com>
2012-07-26 00:43:09 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = 'b85149eb9b9ffd556dd064591539f50074546aa41d11222348756e50dc8199bb0e4d4e418f4170d5949cc4e0dec59ce431c4d7fc6a9c367c87be778c2a44707d'
